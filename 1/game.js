0ed860aff50c3658ee8e8cca0815ea3f509e4bd2
Rory Duncan <robust.rory@gmail.com>
2013-02-02 12:44:00 -0800

    Final commit in foreseeable future.  Basic content accomplished within deadline of OneGameAMonth.

--- 1/game.js
+++ 1/game.js
@@ -85,20 +85,30 @@ var init = function() {
 	};
 	/******************** GAME ********************/
 
-
 	function gameSetControls() {
-		//  Controls
-		gameSetControls = Function("");  //insures only runs once.
-		$(document).bind('keyup.w', game_w_button);
-		$(document).bind('keyup.s', game_s_button);
-		$(document).bind('keyup.a', game_a_button);
-		$(document).bind('keyup.d', game_d_button);
-		$(document).bind('keyup.up', game_w_button);
-		$(document).bind('keyup.down', game_s_button);
-		$(document).bind('keyup.left', game_a_button);
-		$(document).bind('keyup.right', game_d_button);
-		$(document).bind('keyup.return', game_return_button);
-		$(document).bind('keyup.space', game_space_button);
+
+		if (Battle.controlsSet === true) {
+				$(document).unbind('keypress.w', Battle.controls.w);
+				$(document).unbind('keypress.s', Battle.controls.s);
+				$(document).unbind('keypress.a', Battle.controls.a);
+				$(document).unbind('keypress.d', Battle.controls.d);
+				$(document).unbind('keyup.return', Battle.controls.rtrn);
+				Battle.controlsSet = false;
+		 }
+
+		if (Game.controlsSet === false) {
+				$(document).bind('keyup.w', game_w_button);
+				$(document).bind('keyup.s', game_s_button);
+				$(document).bind('keyup.a', game_a_button);
+				$(document).bind('keyup.d', game_d_button);
+				$(document).bind('keyup.up', game_w_button);
+				$(document).bind('keyup.down', game_s_button);
+				$(document).bind('keyup.left', game_a_button);
+				$(document).bind('keyup.right', game_d_button);
+				$(document).bind('keyup.return', game_return_button);
+				$(document).bind('keyup.space', game_space_button);
+				Game.controlsSet = true;
+			}
 	 };
 	function game_w_button() {
 		if ( Player.checkBounderies() === true || Player.checkBounderies() === "AllowUp" ) {
@@ -121,11 +131,11 @@ var init = function() {
 				else return;
 
 			 };
-			moveChar();
 			pan_down();
+			moveChar();
 			Screen.center -= 128;
-			
 			Screen.select();
+			Battle.step();
 		}
 		else { }
 	};
@@ -148,11 +158,12 @@ var init = function() {
 				}
 				else return;
 			 };
-			moveChar();
 			pan_right();
+			moveChar();
 			Player.animState = 3;
 			Screen.center -= 1;
 			Screen.select();
+			Battle.step();
 		 }
 		else { };
 	};
@@ -176,11 +187,12 @@ var init = function() {
 				else return;
 
 			 };
-			moveChar();
 			pan_up();
+			moveChar();
 			Player.animState = 0;
 			Screen.center += 128;
 			Screen.select();
+			Battle.step();
 		 }
 		else { };
 	};
@@ -204,11 +216,12 @@ var init = function() {
 				else return;
 
 			 };
-			moveChar();
 			pan_left();
+			moveChar();
 			Player.animState = 6;
 			Screen.center += 1;
 			Screen.select();
+			Battle.step();
 		 }
 		else { };
 
@@ -274,39 +287,35 @@ var init = function() {
 	/****** Images  ******************************/
 
 	var imagesLoaded = 0,
-			imageCount = 9,
+			imageCount = 8,
 			grass1 = new Image(),
-			stone = new Image(),
 			boulderWall = new Image(),
 			boulderWall2 = new Image(),
 			grass4 = new Image(),
-			tree1 = new Image(),
-			tree2 = new Image(),
+			tree = new Image(),
 			hero_img = new Image(),
 			title_logo1 = new Image(),
-			title_logo2 = new Image();
+			creature1 = new Image();
 
 
 	grass1.src = "res/world/grass_1.png";
-	stone.src = "res/world/stone.png";
 	boulderWall.src = "res/world/boulderwall.png";
 	boulderWall2.src = "res/world/Vertboulderwall.png";
 	grass4.src = "res/world/grass_4.png";
-	tree1.src = "res/world/tree_1.png";
-	tree2.src = "res/world/tree_2.png";
+	tree.src = "res/world/tree_2.png";
 	hero_img.src = "res/heros/c1_spritesheet.png";
 	title_logo1.src = "res/title5.png";
-	title_logo2.src = "res/title4.png";
+	creature1.src = "res/world/dog2.png";
+
+
 	grass1.addEventListener('load', imageLoad, false);
-	stone.addEventListener('load', imageLoad, false);
 	boulderWall.addEventListener('load', imageLoad, false);
 	boulderWall2.addEventListener('load', imageLoad, false);
 	grass4.addEventListener('load', imageLoad, false);
-	tree1.addEventListener('load', imageLoad, false);
-	tree2.addEventListener('load', imageLoad, false);
+	tree.addEventListener('load', imageLoad, false);
 	hero_img.addEventListener('load', imageLoad, false);
 	title_logo1.addEventListener('load', imageLoad, false);
-	title_logo2.addEventListener('load', imageLoad, false);
+	creature1.addEventListener('load', imageLoad, false);
 
 
 
@@ -317,11 +326,12 @@ var init = function() {
 
 
 /****** Objects  
-	█████        ████                        █  
-	█           █        █            █                    █
-	█           █        █████                    █
-	█            █       █          ██       █       █
-	█████       ██████      ████        ██
+███████████████████████████████████████████████████████████████████████████████████████████
+███████████████████████████████████████████████████████████████████████████████████████████
+███████████████████████████████████████████████████████████████████████████████████████████
+███████████████████████████████████████████████████████████████████████████████████████████
+███████████████████████████████████████████████████████████████████████████████████████████
+
 ******************************/
 
 /**/var Color = {};
@@ -365,24 +375,6 @@ var init = function() {
 		}
 		var result = (new Date() - myDate);
 		Game.loadingText = (Map.tiles + " tiles loaded in " + result + " milliseconds.");
-
-	/*	 Make an array of map bounderies to check against later 
-		for (var i = 0, ii = Map.data.length; i < ii; i++) {
-			if (Map.data[i].x === 1 || Map.data[i].x === 2) {
-				Map.edge.push(i);
-			}
-			else if (Map.data[i].x === 127 || Map.data[i].x === 128) {
-				Map.edge.push(i);
-			}
-			else if (Map.data[i].y === 1 || Map.data[i].y === 2) {
-				Map.edge.push(i);
-			}
-			else if (Map.data[i].y === 127 || Map.data[i].y === 128) {
-				Map.edge.push(i);
-			}
-
-		}
-		*/
 		Map.made = true;
 		
 	};
@@ -404,6 +396,7 @@ var init = function() {
 		Screen.dimensions = {};
 		Screen.dimensions.row = 0;
 		Screen.dimensions.col = 0;
+		Screen.overlay = 0;
 		Screen.select = function() {
 			/*			This program selects the tiles to be loaded, and displayed.			*/
 			/*			The tile shown is determined by	Map.data[]							*/
@@ -469,7 +462,6 @@ var init = function() {
 		 };
 		Screen.tilesLoaded = false;
 		Screen.loadTiles = function() { };
-		Screen.move = function() { };
 		Screen.drawTiles = function() {
 			Screen.SetScreenInitially();
 
@@ -507,7 +499,7 @@ var init = function() {
 			for (var i = 0, ii = Screen.selected.length; i < ii; i++) {
 													//drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
 
-				if (Screen.selected[i].image === 1) ctx.drawImage(tree2, 50, 100, 50, 50, ((Screen.selected[i].x)*Map.tileSize), ((Screen.selected[i].y)*Map.tileSize), 50, 50);
+				if (Screen.selected[i].image === 1) ctx.drawImage(tree, 50, 100, 50, 50, ((Screen.selected[i].x)*Map.tileSize), ((Screen.selected[i].y)*Map.tileSize), 50, 50);
 				else continue;
 			}
 		};
@@ -517,7 +509,7 @@ var init = function() {
 					var chance = Math.random();
 					var scaleJitter = 1;
 					if (chance < .08) { scaleJitter = 1 + Math.random()*.02;}
-				if (Screen.selected[i].image === 1) ctx.drawImage(tree2, 0, 0, 150, 100, (Screen.selected[i].x-1)*Map.tileSize, (Screen.selected[i].y-2)*Map.tileSize, 150*scaleJitter, 100*scaleJitter);
+				if (Screen.selected[i].image === 1) ctx.drawImage(tree, 0, 0, 150, 100, (Screen.selected[i].x-1)*Map.tileSize, (Screen.selected[i].y-2)*Map.tileSize, 150*scaleJitter, 100*scaleJitter);
 			}
 		};
 		Screen.SetScreenInitially = function() {
@@ -541,9 +533,11 @@ var init = function() {
 		Player.y = 0;
 		Player.isOnTile = 0;
 		Player.spawnTile = 0;
+		Player.champion = null;
 		Player.make = function() {
 			Player.make = Function("");
-			new Battle.Hero("Player", "Wanderer");
+			playername = window.prompt("What is your name, Adventurer?");
+			Player.champion = new Battle.Hero(playername);
 		 };
 		Player.animState = 0;
 
@@ -562,23 +556,13 @@ var init = function() {
 		};
 		Player.checkBounderies = function() {
 
-			if ((~~Player.x === 14)) {return "AllowRight";}
-			else if ((~~Player.x) === 113) {return "AllowLeft";}
-			else if ((~~Player.y) === 13) {return "AllowDown";}
-			else if ((~~Player.y) === 113) {return "AllowUp";}
+			if ((~~Player.x === 14)) {			return "AllowRight";}
+			else if ((~~Player.x) === 113) {	return "AllowLeft";}
+			else if ((~~Player.y) === 13) {		return "AllowDown";}
+			else if ((~~Player.y) === 113) {	return "AllowUp";}
 			else {return true;}
 
 		 };
-
-
-
-/**/var Entity = {};
-		// Player currently is not added as an entity.
-		Entity.all = [];
-		Entity.add = function(entity) {
-			Entity.all.push(entity);
-		};
-
 
 
 	/**/function Timer() {
@@ -598,45 +582,239 @@ var init = function() {
 	};
 
 /**/var Battle = {};
+		Battle.controlsSet = false;
+		Battle.setControls = function() {
+
+			if (Battle.controlsSet === false) {
+				$(document).unbind('keyup.w', game_w_button);
+				$(document).unbind('keyup.s', game_s_button);
+				$(document).unbind('keyup.a', game_a_button);
+				$(document).unbind('keyup.d', game_d_button);
+				$(document).unbind('keyup.up', game_w_button);
+				$(document).unbind('keyup.down', game_s_button);
+				$(document).unbind('keyup.left', game_a_button);
+				$(document).unbind('keyup.right', game_d_button);
+				$(document).unbind('keyup.return', game_return_button);
+				$(document).unbind('keyup.space', game_space_button);
+				Game.controlsSet = false;
+
+
+				$(document).bind('keypress.w', Battle.controls.w);
+				$(document).bind('keypress.s', Battle.controls.s);
+				$(document).bind('keypress.a', Battle.controls.a);
+				$(document).bind('keypress.d', Battle.controls.d);
+				$(document).bind('keyup.return', Battle.controls.rtrn);
+				Battle.controlsSet = true;
+			}
+		}
+		Battle.controls = {};
+			Battle.controls.w = function(){
+				Game.arrow-=20;
+				if (Game.arrow < 550) { Game.arrow +=60;}
+			 };
+			Battle.controls.a = function(){
+				Game.arrow-=20;
+				if (Game.arrow < 550) { Game.arrow +=60;}
+			 };
+			Battle.controls.s = function(){
+				Game.arrow+=20;
+				if (Game.arrow > 590) { Game.arrow -=60;}
+
+			 };
+			Battle.controls.d = function(){
+				Game.arrow+=20;
+				if (Game.arrow > 590) { Game.arrow -=60;}
+			};
+			Battle.controls.rtrn = function(){
+				if (Game.arrow === 550) {		console.log("Attack!");Battle.abilities.attack();}
+				else if (Game.arrow === 570) {	console.log("Defend!");Battle.abilities.defend();}
+				else if (Game.arrow === 590) {	console.log("Heal!"); Battle.abilities.heal();}
+			};
 		Battle.hero = {}; 
+		Battle.enemy = {};
+		Battle.enemy.recycle = function() {
+
+			this.maxhp = (10+Player.champion.stats.level) + ~~( (Math.random()*10) + Player.champion.stats.level*1.2);
+			this.hp = 0;
+			this.damage = ~~(3 + Math.random()*Player.champion.stats.level);
+			this.speed = ~~(Math.random()*20) + Player.champion.stats.level;
+			this.hp += this.maxhp;
+
+		 };
 		Battle.hero.list = [];
-		Battle.Hero = function(characterName, classTitle) {
+		Battle.Hero = function(characterName) {
 			this.name = characterName;
-			this.class = classTitle;
-			this.level = 0;
-			if (classTitle === "warrior") {
-				this.stats = {
-					str : 15,
-					int : 6,
-					agi : 10
-			 	};
-			 }
-			 if (classTitle === "wizard") {
-				this.stats = {
-					str : 10,
-					int : 25,
-					agi : 5
-			 	};
-			 }
+			this.stats = {
+
+				level : 1,
+				str : 5,
+				int : 5,
+				agi : 5,
+				xp : 0,
+				maxhp :  1,
+				hp : 0,
+				speed : 5,
+				xp2lvl: 30
+			 };
+			this.stats.maxhp = ~~(13 + ( (.75 * this.stats.str) + (this.stats.int / 4) ));
+			this.stats.hp = this.stats.maxhp;
+			this.stats.speed += this.stats.agi/2;
 			data = this;
 			Battle.hero.list.push(data);
 
 
 		 };
-		Battle.hero.make = function(name, className) {
+		Battle.hero.levelup = function() {
+			var player = Player.champion.stats;
+			player.xp = 0;
+			Player.hp = player.maxhp;
+			var statFocus = prompt("You leveled up! \n What stat would you like to increase? \n" + "str: " + player.str + " | int: " + player.int + "| agi: " + player.agi , " str, int, or agi");
+			if (statFocus === "str") player.str +=2;
+			else if (statFocus === "int") player.int +=2;
+			else if (statFocus === "agi") player.agi +=2;
+			player.str +=1;
+			player.int +=1;
+			player.agi +=1;
+			Battle.exit();
+		};
+		var worldX, worldY;
+		Battle.start = function() {
+			worldX = Screen.x;
+			worldY = Screen.y;
+			console.log(Screen.x, Screen.y);
+			Screen.moveToX = (Screen.x)*-1;
+			Screen.moveToY = (Screen.y)*-1;
+			pan();
+			Game.mode = 3;
+			Battle.setControls();
+			Game.arrow = ( (canvas.height - 300 ) + 100);
+
+			Battle.enemy.recycle();
 
 		 };
-		Battle.start = function() {
-			Game.mode = 3;
+		Battle.end = function() {
+
+			Player.champion.stats.xp += ~~(Battle.enemy.maxhp / Player.champion.stats.level);
+			Player.champion.stats.xp += ~~(Player.champion.stats.int/3);
+			if (Player.champion.stats.xp >= Player.champion.stats.xp2lvl)
+			 {
+			 	console.log("Level up!");
+				Battle.hero.levelup();
+			 }
+			else Battle.exit();
+
+
+		};
+		Battle.exit = function() {
+			Game.mode = 2;
+			Battle.stepCount = 100;
+			console.log(worldX, worldY);
+			Screen.moveToX = worldX*0.5;
+			Screen.moveToY = worldY*0.5;
+			gameSetControls();
+			pan();
+
+		};
+		Battle.scale = 1.2;
+		Battle.skew = 1.5;
+		Battle.background = function() {
+
+			var ground = ctx.createLinearGradient(0, 50*8, 0, canvas.height);
+      			ground.addColorStop(0, '#0d2b09');
+      			ground.addColorStop(.15, '#201708');
+      			ground.addColorStop(.8, '#201708');
+      			ground.addColorStop(.8, '#2a2825')
+      			ground.addColorStop(1, '#2a2825');
+			ctx.fillStyle= ground;
+			ctx.fillRect(0, 50*8, canvas.width, canvas.height);
+
+			for (var i = 0, ii = (canvas.width/50); i < ii; i+=1) {
+				ctx.drawImage(grass4, 0, 0, 50, 50, (50*Battle.scale)*i, 50*8, (50*Battle.scale), 50*Battle.scale/2);
+				//ctx.drawImage(grass4, 0, 0, 50, 50, (50*Battle.scale)*i, 50*7.4, (50*Battle.scale), 50*Battle.scale/2);
+			 }
+
+
 		 };
 		Battle.scene = function() {
-			ctx.fillStyle = "#181818";
+
+			if ( ~~(Game.timer.gameTime) % 4 === 0) {ctx.fillStyle = "#6b8cab";}
+			else ctx.fillStyle = "#65839f";
 			ctx.fillRect(0,0, canvas.width, canvas.height);
+			Battle.background();
+			ctx.drawImage(title_logo1, 330, canvas.height-200);
+			Battle.menu.UI();
+			ctx.drawImage(creature1, canvas.width - 250 + Math.random()*5, ( 50*7 + Math.random()*5), (50+(Player.champion.stats.level*0.2)*Battle.scale ), 60+(Player.champion.stats.level*0.2)*Battle.scale  );
+			ctx.drawImage(hero_img, 8*50, 0, 50, 60, 150, 50*7+Math.random()*2, 50*Battle.scale, 60*Battle.scale);
+			//console.log("Battle Frame ");
 
 		 };
+		Battle.menu = {};
+		Battle.abilities = {};
+		Battle.abilities.heal = function() {
+			Player.champion.stats.hp += 1 + ~~(Player.champion.stats.int*0.7);	// heal math
+			var healAmount = 1 + ~~(Player.champion.stats.int*0.7);
+			console.log(Player.champion.name + " healed for  " + healAmount + " health.");
+
+			if (Player.champion.stats.hp > Player.champion.stats.maxhp) { 		// adjust for excess
+			 	Player.champion.stats.hp = Player.champion.stats.maxhp;
+			}
+
+			Game.arrow = 550;													//reset 'pointer'	
+			Battle.checkState();	
+			Battle.enemy.attack();	
+
+		 };
+		Battle.abilities.attack = function() {
+			Battle.enemy.hp -= 1 + ~~(Player.champion.stats.str*0.5) + ~~(Player.champion.stats.agi*0.5);
+			var damageDealt = 1 + ~~(Player.champion.stats.str*0.5) + ~~(Player.champion.stats.agi*0.5);
+			console.log(Player.champion.name + " dealt " + damageDealt + " damage.");
+			Battle.checkState();
+			Battle.enemy.attack();
+		 };
+		Battle.abilities.defend = function() {};
+		Battle.enemy.attack = function() {
+			Player.champion.stats.hp -= Battle.enemy.damage;
+			console.log(" The enemy dealt " + Battle.enemy.damage + " to " + Player.champion.name);
+			Battle.checkState();
+		};
+		Battle.checkState = function() {
+			if (Battle.enemy.hp <= 0) {
+				Battle.end();
+			}
+			else if (Player.champion.stats.hp <= 0 ) Game.mode = 4;
+		};
+		Battle.menu.UI = function() {
+		
+
+			var relPos =  canvas.height-300;
+
+			ctx.fillStyle = " #0a8";
+			ctx.font = "normal 60px PressStart2P";
+			ctx.fillText(Player.champion.stats.hp + "/" + Player.champion.stats.maxhp + " ", 40, 50*8+7);
+			ctx.fillStyle = " #933";
+			ctx.font = "normal 40px PressStart2P";
+			ctx.fillText(Battle.enemy.hp + "/" + Battle.enemy.maxhp + " ", canvas.width-300, 50*8);
+
+			ctx.fillStyle="#bbb";
+			ctx.font = "normal 40px PressStart2P";
+			ctx.fillText( Player.champion.name, 20, 50*7 );
+			
+			ctx.font = "normal 20px PressStart2P";
+			ctx.fillStyle = Color.infotext;
+			ctx.fillText("Attack", 110, relPos+100);
+			ctx.fillText("Defend", 110, relPos+120);
+			ctx.fillText("Heal", 110, relPos+140);
+			Battle.menu.selector();
+
+		};
+		Battle.menu.selector = function() {
+			ctx.fillStyle = Color.infotext;
+			ctx.fillText("→", 80, Game.arrow);
+		 }
 		Battle.stepCount = 100;
 		Battle.step = function() {
-			Battle.stepCount-=Math.random()*12;
+			Player.champion.stats.hp+=1;
+			Battle.stepCount-=Math.random()*20;
 			if (Battle.stepCount <= 0) {
 				Battle.stepCount = 100;
 				Battle.start();
@@ -645,16 +823,18 @@ var init = function() {
 
 
 /**/var Game = {};
+		Game.controlsSet = false;
 		Game.mode = 0;
 		Game.timer = new Timer();
 		Game.drawScreen = function() {	/*		DrawScreen			*/
 
-			console.log("frame");
+			
 			Game.checkState();
-			if (Game.mode === 0) Game.titleScreen();
-			if (Game.mode === 1) Game.load();
-			if (Game.mode === 2) { gameSetControls(); Game.init();}
-			if (Game.mode === 3) { Battle.scene();}
+			if (Game.mode === 0) { Game.titleScreen(); console.log("frame");}
+			if (Game.mode === 1) {Game.load(); console.log("frame");}
+			if (Game.mode === 2) {gameSetControls(); Game.init();  Player.make();  console.log("frame");}
+			if (Game.mode === 3) {Battle.scene(); /*console.log("Battle frame");*/}
+			if (Game.mode === 4) {Game.over();}
 		 };
 		Game.arrow = 275;
 		Game.helpText = "Press ENTER to select.";
@@ -716,6 +896,7 @@ var init = function() {
 
 		Game.init = function() {
 
+
 			ctx.fillStyle = "#254027";
 			ctx.fillRect(Screen.selected.length*(-50), Screen.selected.length*(-50), (Screen.selected.length*50)*2+100, (Screen.selected.length*50)*2+100);
 			//ctx.drawImage(skybox, Screen.selected.length*(-50), Screen.selected.length*(-50), (Screen.selected.length*50)*3+100, (Screen.selected.length*50)*3+100);
@@ -734,7 +915,7 @@ var init = function() {
 		Game.debug.grid = function() {
 			ctx.beginPath();
 			
-			for (var vert = 0, vertCap = 128; vert < vertCap; vert++) {		
+			for (var vert = 0, vertCap = 128; vert < vertCap; vert++) {
 				ctx.moveTo(vert*50,0)		
 				ctx.lineTo(vert*50, 128*50);
 			}
@@ -746,6 +927,18 @@ var init = function() {
 			ctx.strokeStyle = "#111";
 			ctx.stroke();
 		};
+		Game.over = function() {
+			ctx.fillStyle=Color.background;
+			ctx.fillRect(0,0, canvas.width, canvas.height);
+			ctx.drawImage(title_logo1, 320, 180);
+			ctx.fillStyle = "#833";
+			ctx.font = "normal 60px PressStart2P";
+			ctx.fillText("Game Over", 220, 400);
+			ctx.font = "normal 10px PressStart2P";
+			ctx.fillStyle = Color.infotext;
+			ctx.fillText("Maybe if you hadn't named yourself " + Player.champion.name + " you would've done better.", 120, 480);
+
+		}
 
 
 
